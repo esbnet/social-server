30749157dc0f20bed1f1d3375c08b9a864b34152
Edmilson Soares <esbnet@gmail.com>
2022-03-10 22:06:58 -0300

    delete user - ok

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,7 @@ const bcrypt = require("bcrypt");
 
 //update user
 router.put("/:Id", async (req, res) => {
-
 	if (req.body.userId === req.params.Id || req.user.isAdmin) {
-
 		if (req.body.password) {
 			try {
 				const salt = await bcrypt.genSalt(10);
@@ -14,23 +12,34 @@ router.put("/:Id", async (req, res) => {
 			} catch (err) {
 				return res.status(500).json(err);
 			}
-
-			try {
-				const user = await User.findByIdAndUpdate(req.params.Id, {
-					$set: req.body,
-				});
-				res.status(200).json("Usuário atualizado com sucesso!");
-			} catch (err) {
-				return res.status(500).json(err);
-			}
-
-		} else {
-			return res.status(403).json("Você só pode atualizar sua conta!");
 		}
+
+		try {
+			const user = await User.findByIdAndUpdate(req.params.Id, {
+				$set: req.body,
+			});
+			res.status(200).json("Usuário atualizado com sucesso!");
+		} catch (err) {
+			return res.status(500).json(err);
+		}
+	} else {
+		return res.status(403).json("Você só pode atualizar sua conta!");
 	}
 });
 
 //delete user
+router.delete("/:id", async (req, res) => {
+	if (req.body.userId === req.params.id || req.user.isAdmin) {
+		try {
+			const user = await User.findByIdAndDelete(req.params.id);
+			res.status(200).json("Usuário deletado com sucesso!");
+		} catch (err) {
+			return res.status(500).json(err);
+		}
+	} else {
+		return res.status(403).json("Você só pode deletar sua conta!");
+	}
+});
 
 //get user
 router.get("/:id", async (req, res) => {
