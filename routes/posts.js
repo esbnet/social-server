c031ab1c698d0b1d6b35ee0e8061c05c88c7e36c
Edmilson Soares <esbnet@gmail.com>
2022-03-11 10:21:24 -0300

    readme edit

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 const Post = require("../models/Post");
+const User = require("../models/User");
 
 //create a new post
 router.post("/", async (req, res) => {
@@ -72,9 +73,30 @@ router.put("/:id/likes", async (req, res) => {
 	}
 });
 
-
 //get a post by id
+router.get("/:id", async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+		res.status(200).json(post);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
 
 //get timeline posts
+router.get("/timeline/all", async (req, res) => {
+	try {
+		const currentUser = await User.findById(req.body.userId);
+		const userPosts = await Post.find({ userId: currentUser._id });
+		const friendsPosts = await Promise.all(
+			currentUser.followings.map((friendId) => {
+				return Post.find({ userId: friendId });
+			})
+		);
+		res.json(userPosts.concat(...friendsPosts));
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
 
 module.exports = router;
