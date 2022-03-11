893299d5ae4bf44c52e658e5a656952ccdb45e12
Edmilson Soares <esbnet@gmail.com>
2022-03-11 07:56:02 -0300

    update post - ok

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const Post = require("../models/Post");
 
 //create a new post
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
 	const newPost = new Post(req.body);
 	try {
 		const savedPost = await newPost.save();
@@ -12,8 +12,29 @@ router.get("/", async (req, res) => {
 		res.status(500).json(error);
 	}
 });
- 
+
 //update a post
+router.put("/:id", async (req, res) => {
+	const post = await Post.findById(req.params.id);
+
+	try {
+		if (post.userId === req.body.userId) {
+			const updatedPost = await Post.findByIdAndUpdate(
+				req.params.id,
+				req.body,
+				{
+					new: true,
+				}
+			);
+			res.status(200).json("Post alterado com sucesso!");
+		} else {
+			res.status(403).json("Você só pode alterar seu post");
+		}
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
 //delete a post
 //like a post
 //get a post by id
